95d316cc43cc9d1ed20e82942e12032e94bbc0fb
mawenqing1 <529837505@qq.com>
2021-05-11 20:41:48 +0800

    feat: 发布到 github.io

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,60 @@
+const moment = require('moment');
+
+module.exports = {
+    base: '/blog/',
+    title: "热爱生活喜欢睡觉的博客",
+    description: "热爱生活 喜欢睡觉的博客",
+    head: [
+        ['link', { rel: 'icon', href: '/assets/img/favicon.ico' }],
+        ['meta', { name: 'author', content: '热爱生活 喜欢睡觉' }],
+        ['meta', { name: 'keywords', content: '热爱生活 喜欢睡觉的博客  vuepress搭建个人博客  IT博客  前端学习' }]
+    ],
+    plugins: [
+        [
+            '@vuepress/last-updated',
+            {
+                transformer: (timestamp, lang) => {
+                    const moment = require('moment')
+                    moment.locale("zh-cn")
+                    return moment(timestamp).format("LLLL")
+                }
+            }
+        ]
+    ],
+    themeConfig: {
+        logo: '/assets/img/logo.png',
+        lastUpdated: '更新时间',
+        nav: [
+            { text: '主页', link: '/' },
+            { text: '先导', link: '/synopsis/guide' },
+            {
+                text: '前端学习',
+                ariaLabel: ' ',
+                items: [
+                    { text: 'HTML', link: '/frontend/HTML/' },
+                    { text: 'JavaScript', link: '/frontend/JavaScript/' },
+                    { text: 'CSS', link: '/frontend/CSS/' },
+                    { text: 'Vue', link: '/frontend/Vue/' }
+                ]
+            },
+            {
+                text: '更多',
+                ariaLabel: ' ',
+                items: [
+                    { text: 'VuePress官网', link: 'https://vuepress.vuejs.org/zh/' },
+                    { text: 'MarkDown手册', link: 'http://www.markdown.cn/' }
+                ]
+            },
+            { text: 'GitHub', link: 'https://github.com/mawenqing1/blog' },
+        ],
+        sidebar: {
+            '/synopsis/': [
+                'guide',
+                'about-me',
+                'other'
+            ],
+
+        },
+    }
+
+}
---
+++ docs/README.md
@@ -0,0 +1,15 @@
+---
+home: true
+heroImage: /hero.png
+# heroText: 热爱生活喜欢睡觉的博客
+tagline: 没事自己瞎写点博文
+actionText: 查看笔记 →
+actionLink: /synopsis/guide
+footer: MIT Licensed | Copyright © 2018-present Evan You
+---
+
+## 关于笔记
+
+::: danger
+emmmm.... 本博主非技术性博主，所以懂的都懂....欢迎各位大佬留言指正。
+:::
